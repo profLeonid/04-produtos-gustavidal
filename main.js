6605cbfb0ad885b2b65ce5b4f59731cb9ea97ba4
GUSTAVO VIDAL DE ABREU <25203686@dsn127m1246729.cfp127aluno.local>
2026-02-24 15:19:46 -0300

    feat: validar entrada

--- main.js
+++ main.js
@@ -3,10 +3,16 @@
 function adicionarProduto() {
     const produto = document.getElementById('produto')
     const lista = document.getElementById('lista')
-
     const span = document.createElement('span')
-    span.textContent = produto.value
-    span.className = 'bg-blue-200 px-8 py-2'
 
-    lista.appendChild(span)
+    if (produto.value == '') {
+        alert('O campo de produtos não pode estar vazio!')
+    } else if (!isNaN(produto.value)){
+        alert('O campo de produtos não pode conter somente números!')
+    } else {
+        span.textContent = produto.value
+        span.className = 'bg-blue-200 px-8 py-2'
+
+        lista.appendChild(span)
+    }
 }
