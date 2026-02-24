bb382fd9ac4f9716a7899f8ebe49e98e516abd84
GUSTAVO VIDAL DE ABREU <25203686@dsn127m1246729.cfp127aluno.local>
2026-02-24 16:31:58 -0300

    feat: finalizar projeto

--- main.js
+++ main.js
@@ -1,18 +1,45 @@
 'use strict'
 
 function adicionarProduto() {
+    const codigo = document.getElementById('codigo')
     const produto = document.getElementById('produto')
+    const quantidade = document.getElementById('quantidade')
+    
+    const spanCodigo = document.createElement('span')
+    const spanProduto = document.createElement('span')
+    const spanQtde = document.createElement('span')
+    
     const lista = document.getElementById('lista')
-    const span = document.createElement('span')
 
-    if (produto.value == '') {
-        alert('O campo de produtos não pode estar vazio!')
-    } else if (!isNaN(produto.value)){
+    const linha = document.createElement('div')
+
+    if (isNaN(codigo.value)) {
+        alert('O código deve ser informada somente por números!')
+    } else if (!isNaN(produto.value)) {
         alert('O campo de produtos não pode conter somente números!')
+    } else if (isNaN(quantidade.value)) {
+        alert('A quantidade deve ser informada somente por números!')
+    } else if (codigo.value == '' || produto.value == '' || quantidade.value == '') {
+        alert('Preencha todos os campos!')
     } else {
-        span.textContent = produto.value
-        span.className = 'bg-blue-200 px-8 py-2'
+        spanCodigo.className = 'bg-blue-200 px-8 py-2 w-5s h-1s'
+        spanProduto.className = 'bg-blue-200 px-8 py-2 w-5s h-1s'
+        spanQtde.className = 'bg-blue-200 px-8 py-2 w-5s h-1s'
 
-        lista.appendChild(span)
+        spanCodigo.textContent = codigo.value
+        spanProduto.textContent = produto.value
+        spanQtde.textContent = quantidade.value
+
+        linha.className = 'flex gap-2'
+
+        linha.appendChild(spanCodigo)
+        linha.appendChild(spanProduto)
+        linha.appendChild(spanQtde)
+
+        lista.appendChild(linha)
+
+        codigo.value = ''
+        produto.value = ''
+        quantidade.value = ''
     }
 }
